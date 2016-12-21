6e6e9be7095c773e92c7c3145fdad001a9c72da9
m-yuhas <m-yuhas@qq.com>
2016-12-21 14:05:52 -0800

    Adding swift testing code

---
+++ swift_test/mathParser.swift
@@ -0,0 +1,7 @@
+// An algebraic parser in swift, relies on Access to a dictionary of Variables
+// Definition of Variable object available in varObject.swift
+// (C) 2016 Michael Yuhas
+
+func parseExpression(expression : String) {
+  return
+}
---
+++ swift_test/readFile.h
@@ -0,0 +1,4 @@
+// This is the corresponding header to readFile.c
+// (C) 2016 Michael Yuhas
+
+char* readFile( const char* pathToFile );
