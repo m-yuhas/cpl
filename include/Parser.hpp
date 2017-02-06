0eb6309c1e830120b2831d5b6afca05cdc575188
m-yuhas <m-yuhas@qq.com>
2017-02-05 22:48:53 -0800

    Commiting prototype for Parser class

---
+++ include/Parser.hpp
@@ -0,0 +1,15 @@
+// This class includes various parsers for mathematical expressions
+// (C) 2016 Michael Yuhas
+
+#pragma once
+#include "../include/InvalidOperationException.hpp"
+#include "../include/VarObject.hpp"
+#include "../include/UnicodeString.hpp"
+
+class Parser
+{
+  public:
+    static VarObject evaluateExpression( UnicodeString expression );
+  private:
+    static VarObject evaluateAtom( UnicodeString expression );
+};
---
+++ src/Parser.cpp
@@ -0,0 +1,15 @@
+// This class includes various parsers for mathematical expressions
+// (C) 2016 Michael Yuhas
+
+#pragma once
+#include "../include/Parser.hpp"
+
+VarObject Parser::evaluateExpression( UnicodeString expression )
+{
+
+}
+
+VarObject Parser::evaluateAtom( UnicodeString expression )
+{
+  
+}
