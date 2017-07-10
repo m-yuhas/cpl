bce07681c221bba77ca669d8a5694df2061ac945
m-yuhas <m-yuhas@qq.com>
2017-07-10 00:24:14 -0700

    Comment Parsing Refactor

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
       fmt.Println("错误：不能开文件")
       panic(err)
     }
-    defer f.Close()
+    defer f.Close() //TODO: make sure that this is the right way to open/close files
     var lines []string
     scanner := bufio.NewScanner(f)
     for scanner.Scan() {
@@ -53,6 +53,10 @@ func main() {
     if err := scanner.Err(); err != nil {
       fmt.Println(os.Stderr,err)
     }
+
+
+    lines = find_comments(lines)
+
     variableMap := []map[string]variable.Variable{}
     variableMap = append(variableMap, map[string]variable.Variable{})
 
@@ -111,5 +115,66 @@ func cli_main() {
     }
     userInput = strings.Replace(userInput,"\r\n","",-1)
     parser.ParseLine(userInput)
+  }
+}
+
+/*
+find_comments - finds and removes comments from an array of lines
+Arguments:
+  str_arr - Array of strings
+Returns:
+  []string - Modified Array of strings with comments removed
+*/
+func find_comments( str_arr []string ) []string {
+    var output_str_arr []string
+    // Loop through array and search for commented blocks
+    for i := 0; i < len(str_arr); i++ {
+        // Check for start of commented block
+        if strings.HasPrefix(str_arr[i],"注释：") || strings.HasPrefix(str_arr[i],"注释:") {
+          var j int
+          // Loop through lines to search for end of commented block
+          for j = i+1; j < len(str_arr); j++ {
+            // If the end is found stop the loop
+            if strings.HasSuffix(str_arr[j],"结束注释") {
+              break
+            }
+          }
+          // If the end was found, update the line counter to the current position
+          if j < len(str_arr) {
+            i = j
+          }
+        } else if strings.HasSuffix(str_arr[i],"结束注释") {
+          fmt.Println(messages.CommentEndWithoutStart)
+          os.Exit(0)
+        } else {
+          output_str_arr = append(output_str_arr,str_arr[i])
+        }
+    }
+    return output_str_arr
+}
+
+/*
+strip_whitespace - remove unnecessary whitespace from string array
+Arguments:
+  str_arr - Array of strings
+Returns:
+  []string - Array of strings with whitespace removed
+*/
+func strip_whitespace( str_arr []string ) []string {
+  var output_str_arr []string
+  // Loop through array and remove unneeded spaces line by line
+  for i := 0; i < len(str_arr); i++ {
+    input_line := []rune(str_arr[j])
+    var output_line []rune
+    // Loop through runes in the line and find unneeded space
+    for j := 0; j < len(input_line); i++ {
+    
+      if input_line[j] == ' ' {
+        continue
+      } else {
+        output_line = append(input_line[j])
+      }
+    }
+    output_str_arr = append(output_str_arr,string(output_line))
   }
 }
--- messages/messages.go
+++ messages/messages.go
@@ -10,3 +10,5 @@ package messages
 
 const CLIHeaderText string = "欢迎中华电脑语言第0.2版本!\n©2017 － 迈克尔 余哈斯"
 const CLIInputError string = "错误：输入错误发生了、请查下你刚输入的句"
+
+const CommentEndWithoutStart string = "错误：结束解没有链接的‘解：’"
